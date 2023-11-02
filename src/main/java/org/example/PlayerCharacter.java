9970075b71541ff397df10c534578eeb70b2ddf0
Sergej Jaudszims <redtape9688@gmail.com>
2023-11-02 15:05:11 +0100

    implement getX, getY, return0_whenGetYReturns0 Test

--- src/main/java/org/example/PlayerCharacter.java
+++ src/main/java/org/example/PlayerCharacter.java
@@ -1,7 +1,19 @@
 package org.example;
 
-public class Main {
+public class PlayerCharacter {
+    protected int x = 0;
+    protected int y = 0;
+
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+
 }
--- src/test/java/org/example/PlayerCharacterTest.java
+++ src/test/java/org/example/PlayerCharacterTest.java
@@ -4,9 +4,23 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MainTest {
+class PlayerCharacterTest {
 
     @Test
-    
+    void return0_whenGetXReturns0() {
+        PlayerCharacter newPlayer = new PlayerCharacter();
+        int expected = 0;
+        int actual = newPlayer.getX();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void return0_whenGetYReturns0() {
+        PlayerCharacter newPlayer = new PlayerCharacter();
+        int expected = 0;
+        int actual = newPlayer.getY();
+        assertEquals(expected, actual);
+    }
+
 
 }
